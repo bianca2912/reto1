dbe166e83910eb8f6cfef99975a41edf80642acc
bianca2912 <biancasimina01@gmail.com>
2024-10-29 10:31:14 +0100

    he hecho la suma
    Merge branch 'main' of https://github.com/bianca2912/reto1.git into main

--- reto1/src/reto1/funcion1.java
+++ reto1/src/reto1/funcion1.java
@@ -1,5 +1,9 @@
 package reto1;
 
 public class funcion1 {
-
+	
+	public static int sumar (int a, int b) {
+		return a + b;
+	}
+	
 }
--- reto1/src/reto1/retos.java
+++ reto1/src/reto1/retos.java
@@ -1,10 +1,80 @@
 package reto1;
+import reto1.funcion1;
+import reto1.funcion2;
+import java.util.Scanner;
 
 public class retos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola.");
+		int a = 0;
+		int b = 0;
+		int menu = 9;
+		Scanner scanner = new Scanner (System.in);
+		boolean valido;
+		
+		do {
+			System.out.println("Elige la operación:");
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("5. Par");
+			System.out.println("6. Impar");
+			System.out.println("7. Positivo");
+			System.out.println("8. Negativo");
+			System.out.println("0. Salir");
+			String input = scanner.nextLine();
+			try {
+				menu = Integer.parseInt(input);
+				if (menu >=0 && menu<=8) {
+					valido = true;
+				} else {
+					System.out.println("Número incorrecto.");
+					valido = false;
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Número no reconocido.");
+				valido = false;
+			}
+		} while (!valido);
+		
+		do {
+			System.out.println("Introduce el primer número: ");
+			String input = scanner.nextLine();
+			try {
+				a = Integer.parseInt(input);
+				valido = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Número no reconocido.");
+				valido = false;
+			}
+		} while (!valido);
+		
+		do {
+			System.out.println("Introduce el primer número: ");
+			String input = scanner.nextLine();
+			try {
+				b = Integer.parseInt(input);
+				valido = true;
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Número no reconocido.");
+				valido = false;
+			}
+		} while (!valido);
+		
+			switch (menu) {
+			case 1: {
+				int resultado = funcion1.sumar(a, b);
+				System.out.println ("La suma entre: " + a + " + " + b + " es: " + resultado);
+			}
+			default:
+				break;
+			}
 	}
-
 }
+
+
